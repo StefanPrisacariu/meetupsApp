bbde7de269b7e05ac6320f970ace08e85190968b
Stefan Prisacariu <spr@miracle.dk>
2022-01-17 15:17:45 +0200

    Aligned Cards

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,29 +2,31 @@ import "./Main.css";
 
 function Main() {
   return (
-    <div className="card">
-      <div className="card-content">
-        <div>Test</div>
-      </div>
+    <div className="main-content">
+      <div className="align">
+        <div className="card">
+          <div>Test</div>
+        </div>
 
-      <div className="card-content">
-        <div>Test</div>
-      </div>
+        <div className="card">
+          <div>Test</div>
+        </div>
 
-      <div className="card-content">
-        <div>Test</div>
-      </div>
+        <div className="card">
+          <div>Test</div>
+        </div>
 
-      <div className="card-content">
-        <div>Test</div>
-      </div>
+        <div className="card">
+          <div>Test</div>
+        </div>
 
-      <div className="card-content">
-        <div>Test</div>
-      </div>
+        <div className="card">
+          <div>Test</div>
+        </div>
 
-      <div className="card-content">
-        <div>Test</div>
+        <div className="card">
+          <div>Test</div>
+        </div>
       </div>
     </div>
   );
