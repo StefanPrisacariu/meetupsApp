87cbea88b77e324ef1333b8a4dcc392a572eb76f
Stefan Prisacariu <spr@miracle.dk>
2022-01-25 14:21:07 +0200

    FIXED Error React

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -3,7 +3,7 @@ import "./Carousel.css";
 function Carousel() {
   return (
     <div className="carousel">
-      <div className='carousel-box'>
+      <div className="carousel-box">
         <p>Test</p>
       </div>
     </div>
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,15 +1,17 @@
-import './Navigation.css'
+import "./Navigation.css";
 
 function Navigation() {
-    return <nav>
-        <ul>
-            <li>Home</li>
-            <li>Page 1</li>
-            <li>Page 2</li>
-            <li>Page 3</li>
-            <li>Page 4</li>
-        </ul>
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Page 1</li>
+        <li>Page 2</li>
+        <li>Page 3</li>
+        <li>Page 4</li>
+      </ul>
     </nav>
+  );
 }
 
 export default Navigation;
