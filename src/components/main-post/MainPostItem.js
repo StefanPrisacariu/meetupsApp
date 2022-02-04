a5ffc5d51fd78062598ab3b8a7539001636fd3d5
Stefan Prisacariu <spr@miracle.dk>
2022-02-04 14:19:03 +0200

    Working Overlay

--- src/components/main-post/MainPostItem.js
+++ src/components/main-post/MainPostItem.js
@@ -1,24 +1,37 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import PostsContext from "../../store/PostsContext";
 import Card from "../Card/Card";
 import "./MainPostItem.css";
+import Overlay from "../overlay/Overlay";
 
 function MainPostItem(props) {
   const postsCtx = useContext(PostsContext);
+  const [hideOverlay, setHideOverlay] = useState(true);
+
   return (
-    <Card>
-      <div className="image">
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className="title">
-        <h2>{props.title}</h2>
-      </div>
-      <div className="text">
-        <p>{props.text}</p>
-      </div>
-      <button className="delete" onClick={() => postsCtx.removePost(props.id)}>X</button>
-    </Card>
-    
+    <>
+      <Card>
+        <div className="image">
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className="title">
+          <h2>{props.title}</h2>
+        </div>
+        <div className="text">
+          <p>{props.text}</p>
+        </div>
+        <button
+          className="delete"
+          onClick={() => setHideOverlay(!hideOverlay) /*postsCtx.removePost(props.id)*/}
+        >
+          X
+        </button>
+      </Card>
+      {
+        hideOverlay === false && 
+        <Overlay toggleOverlay={() => setHideOverlay(!hideOverlay)} elem={postsCtx} elemId={props.id}/>
+      }
+    </>
   );
 }
 
--- src/components/overlay/Overlay.js
+++ src/components/overlay/Overlay.js
@@ -1,9 +1,19 @@
 import "./Overlay.css";
 
-function Overlay() {
-    return <div className="darkened">
-
+function Overlay({ elem, elemId, toggleOverlay }) {
+  return (
+    <div className="darkened">
+      <div className="check-box">
+        <div className="container">
+          <h2>Are you sure you want to delete this post?</h2>
+          <div className="buttons">
+            <button className="delete-btn" onClick={() => elem.removePost(elemId)}>Delete</button>
+            <button className="cancel-btn" onClick={toggleOverlay}>Cancel</button>
+          </div>
+        </div>
+      </div>
     </div>
+  );
 }
 
 export default Overlay;
