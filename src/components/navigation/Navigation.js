5d738826cd3f15ef980b16be34ba8867e230e673
Stefan Prisacariu <spr@miracle.dk>
2022-02-08 15:23:01 +0200

    NAvigation Menu Mobile Improvement

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import "./Navigation.css";
 
 function Navigation() {
+  const [hideMenu, setHideMenu] = useState(true);
+
   return (
     <nav>
-      <ul>
-        <li>Home</li>
-        <li>Page 1</li>
-        <li>Page 2</li>
-        <li>Page 3</li>
-        <li>Page 4</li>
-      </ul>
+      <button onClick={() => setHideMenu(!hideMenu)}>Menu</button>
+
+      {hideMenu === false && (
+        <ul toggleMenu={()=> setHideMenu(!hideMenu)}>
+          <li>Home</li>
+          <li>Page 1</li>
+          <li>Page 2</li>
+          <li>Page 3</li>
+          <li>Page 4</li>
+        </ul>
+      )}
     </nav>
   );
 }
